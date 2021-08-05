6a0215fa54ee3836be5ba28f9ccac922d76d6e7f
davidballezaa <davidballezaa@gmail.com>
2021-08-04 17:10:59 -0700

    finished with discounts styles and added event listener to get rid of button

--- discounts.js
+++ discounts.js
@@ -5,12 +5,19 @@ function finalPrice(price, discount){
 }
 //interaction with html
 
-function calculateDiscount(){
-    const price_input = document.getElementById("discount-original-price");
+const price_input = document.getElementById("discount-original-price");
+const discount_input = document.getElementById("discount-discount");
+
+
+function getFinalPrice() {
     const price_value = price_input.value;
-    const discount_input = document.getElementById("discount-discount");
     const discount_value = discount_input.value;
     const final_price = finalPrice(price_value, discount_value);
     const priceResult_span = document.getElementById("priceResult");
     priceResult_span.innerText = final_price;
 }
+
+price_input.addEventListener("change", getFinalPrice);
+price_input.addEventListener("keyup", getFinalPrice);
+discount_input.addEventListener("change", getFinalPrice);
+discount_input.addEventListener("keyup", getFinalPrice);
