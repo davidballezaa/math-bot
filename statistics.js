787975bec71460a38c732bf890fa5c6072e95c24
davidballezaa <davidballezaa@gmail.com>
2021-08-05 21:49:13 -0700

    removed unnecesary console.log and shortened the line that converts statistics input string to array

--- statistics.js
+++ statistics.js
@@ -9,9 +9,8 @@ function promedio(list){
 //interaction with HTML
 function calcularPromedio(){
     const input = document.getElementById("statistics-input");
-    const input_value = (input.value.trim()).split(' ');
+    const input_value = input.value.trim().split(' ');
     const number_input_value = input_value.map(value => parseFloat(value));
-    console.log(number_input_value);
     const resultado = promedio(number_input_value);
     const statisticsResultTag = document.getElementById("statisticsResult");
     statisticsResultTag.innerText = resultado;
