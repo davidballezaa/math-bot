a1284df808a36f100c888f98a777e6e0755e8ead
davidballezaa <davidballezaa@gmail.com>
2021-08-05 21:11:05 -0700

    finished with promedio script

--- statistics.js
+++ statistics.js
@@ -1,14 +1,17 @@
 //initial functions
 function promedio(list){
-    list.sort();
+    var sumaLista = 0;
+    list.forEach(value => sumaLista+=value);
+    return (sumaLista / list.length).toFixed(5); 
 }
 
 
 //interaction with HTML
 function calcularPromedio(){
     const input = document.getElementById("statistics-input");
-    const input_value = (input.value).split('-');
-    const number_input_value = input_value.map(value => parseInt(value));
+    const input_value = (input.value.trim()).split(' ');
+    const number_input_value = input_value.map(value => parseFloat(value));
+    console.log(number_input_value);
     const resultado = promedio(number_input_value);
     const statisticsResultTag = document.getElementById("statisticsResult");
     statisticsResultTag.innerText = resultado;
